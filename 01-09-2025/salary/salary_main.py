e580120a7d07a71a70f0f42accd418bcff86d3d2
medimaha <medimahalakshmi@gmail.com>
2025-09-01 16:33:42 +0530

    Menu Updated

--- 01-09-2025/salary/salary_main.py
+++ 01-09-2025/salary/salary_main.py
@@ -1,22 +1,52 @@
 from salary_manager import create_salary,read_all,read_by_salary
 from salary_manager import salaries,update,delete_by_salary
 
-create_salary(1000)
-create_salary(5000)
-create_salary(8000)
-create_salary(3000)
+def menu():
+    message='''
+     1-Create Salary
+     2-Read By Salaries
+     3-Read By Salary
+     4-Update
+     5-Delete
+     6-Exit/Logout
+    '''
+    choice = int(input(message))
+    if choice == 1:
+        salary=int(input('salary:'))
+        create_salary(salary)
+    elif choice==2:
+          result_salaries=read_all()
+          print('salaries:')
+          for salary in result_salaries:
+             print(salary)
+    elif choice ==3:
+        salary = int(input('search salary'))
+        index=read_by_salary(salary)
+        if salary == -1:
+           print('salary is not found')
+        else:
+           print(f' Salary is at index{index}')
+    elif choice ==4:
+        old_salary=int(input('salary to update:'))
+        new_salary=int(input('New Salary'))
+        update(old_salary,new_salary)
 
-result_salaries = read_all()
+    elif choice == 5:
+         salary=int(input('salary to delete:'))
+         delete_by_salary(salary)
 
-for salary in result_salaries: # Corrected line: removed the parentheses after arr
-    print(salary)
+    elif choice==6:
+       print("Exit/Logout")
 
-print(read_by_salary(8000))
-print(read_by_salary(4000))
-print(salaries[read_by_salary(8000)])
+    return choice
+   
 
-update(8000,8500)
-print(read_all())
+def menus():
+    print("Salary Management App")
+    choice=menu()
+    while choice!=6:
+        choice=menu()
+    print("Thank yo for using App")
 
-delete_by_salary(1000)
-print(read_all())
+
+menus()
